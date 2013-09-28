129ea838796bf4933773083184e95c1ac32882fb
tptak <tomasz.ptak@gmail.com>
2013-09-28 06:44:18 +0200

    Adding models

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,9 @@
 __author__ = 'tptak'
 
+from django.contrib import admin
+from models import *
 
+admin.site.register(MemoUser)
+admin.site.register(Game)
+admin.site.register(Configuration)
+admin.site.register(Statistic)
--- game/models.py
+++ game/models.py
@@ -7,5 +7,42 @@ class MemoUser(AbstractUser):
 
 # Create your models here.
 class Game(models.Model):
-    pass
+    STATUS_VALUES = [
+        ('IN', 'Initial'),
+        ('WA', 'WaitingForPlayers'),
+        ('PR', 'InProgress'),
+        ('FI', 'Finished'),
+        ('CA', 'Cancelled'),
 
+    ]
+    state = models.TextField(max_length=2, null=False, choices=STATUS_VALUES, default=STATUS_VALUES[0][0])
+    comment = models.TextField(max_length=255, null=True)
+    host = models.ForeignKey(MemoUser, related_name='hosted_games_set')
+    players = models.ManyToManyField(MemoUser)
+
+
+class Statistic(models.Model):
+    STAT_TYPES = [
+        ('SCO', 'Score'),
+        ('TLE', 'TimeLeft'),
+        ('FIN', 'FinalResult'),
+        ('CTI', 'CreateTime'),
+        ('STI', 'StartTime'),
+        ('FTI', 'FinishTime'),
+    ]
+    game = models.ForeignKey(Game, null=False)
+    type = models.TextField(max_length=3, null=False, choices=STAT_TYPES)
+    player = models.ForeignKey(MemoUser, null=True)
+    value = models.TextField(max_length=10, null=False)
+
+
+class Configuration(models.Model):
+    CONFIG_TYPES = [
+        ('SIZ', 'Size'),
+        ('MTI', 'MoveTime'),
+        ('TTI', 'TotalTime'),
+        ('PRI', 'Private'),
+    ]
+    game = models.ForeignKey(Game, null=False)
+    type = models.TextField(max_length=3, null=False, choices=CONFIG_TYPES)
+    value = models.TextField(max_length=10, null=False)
