a666beccb5197bec7247356df605890b62c604a0
tptak <tomasz.ptak@gmail.com>
2013-09-28 12:23:24 +0200

    Added sample static page to play around with UI

--- game/views.py
+++ game/views.py
@@ -1,7 +1,12 @@
 # Create your views here.
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.views.generic import TemplateView
 
 
 def home_view(request):
     return render_to_response('index.html',{}, context_instance=RequestContext(request))
+
+
+class GameDemoView(TemplateView):
+    template_name = "game_demo_ui.html"
