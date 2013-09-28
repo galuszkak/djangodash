6d8aa0305e12f2ad7a45416d8aef1c3039ed8620
Tomek <tomek@audioslave.(none)>
2013-09-28 12:16:44 +0200

    fix the fakup

--- memo/urls.py
+++ memo/urls.py
@@ -8,7 +8,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
 
     url(r'^$', 'game.views.home_view', name='home'),
-    url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
     # url(r'^memo/', include('memo.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
