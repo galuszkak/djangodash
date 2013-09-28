ab914337ab3bdfd798f8aedfcb03c0c44cc15359
Kamil Gałuszka <galuszkak@gmail.com>
2013-09-28 22:21:57 +0200

    update urls

--- memo/urls.py
+++ memo/urls.py
@@ -22,6 +22,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^%s(?P<path>.*)$' % re.escape(settings.STATIC_URL.lstrip('/')),
-        'django.views.static.serve', document_root=settings.STATIC_ROOT),
+        'django.views.static.serve', {'document_root':settings.STATIC_ROOT}),
 )
 
