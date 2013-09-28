010eb04633bd5601286e3b890b1b7b4d98708c49
Kamil Gałuszka <galuszkak@gmail.com>
2013-09-28 03:27:21 +0200

    added Heroku working

--- memo/urls.py
+++ memo/urls.py
@@ -1,16 +1,15 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
+
     # url(r'^$', 'memo.views.home', name='home'),
     # url(r'^memo/', include('memo.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
