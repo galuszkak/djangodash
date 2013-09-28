b9a12e13e1d764f0accafea808793121e2dc3d19
Kamil Gałuszka <galuszkak@gmail.com>
2013-09-28 04:16:10 +0200

    added all auth

--- game/views.py
+++ game/views.py
@@ -1,4 +1,7 @@
 # Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 
-de
+def home_view(request):
+    return render_to_response('index.html',{}, context_instance=RequestContext(request))
